89a7f6ab7cedb4682db8ea2583279c74cf5df0fc
zesciati <zakyabiyyu2003@gmail.com>
2026-02-03 11:57:35 +0700

    feat: menstransisikan project blog preview card

---
+++ advice generator app/src/blog-preview-card.jsx
@@ -0,0 +1,33 @@
+import IllustrationArticle from "./assets/illustration-article.svg";
+
+export default function blogPreviewCard() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+      </head>
+      <body className="bg-(--yellow) font-(family-name:--figtree)">
+        <main className="flex justify-center items-center min-h-screen flex-col">
+          <div className="p-6 bg-(--white) rounded-[20px] mx-6">
+            <img
+              src={IllustrationArticle}
+              alt="illustration-article"
+              className="rounded-[10px] "
+            />
+            <p className="font-extrabold bg-(--yellow) w-max mt-6 px-3 py-1 rounded-sm">
+              Learning
+            </p>
+            <p className="my-3"> Published 21 Dec 2023</p>
+            <h3 className="text-xl font-extrabold">HTML & CSS foundations</h3>
+            <p className="text-sm text-(--gray-500)">
+              These languages are the backbone of every website, defining
+              structure, content, and presentation.{" "}
+            </p>
+          </div>
+        </main>
+      </body>
+    </html>
+  );
+}
--- advice generator app/src/main.jsx
+++ advice generator app/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
+import BlogPreviewCard from './blog-preview-card.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
+    <BlogPreviewCard />
   </StrictMode>,
 )
